82c0dac87437d6dae14d36c7ce19673ceba88e73
litlle-pig <semenlegostav@gmail.com>
2024-04-01 08:37:11 +0600

    nothing

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    {{-- <link href="{{ asset('css/input.css') }}" rel="stylesheet"> --}}
+    <link href="css/app.css" rel="stylesheet">
+    {{-- <link href="css/input.css" rel="stylesheet"> --}}
 
     <title>@yield('title')</title>
 </head>
@@ -15,6 +15,7 @@
 <body>
     <div class="wrapper">
         @yield('content')
+        <p class="text-blue-700">blue</p>
     </div>
 </body>
 
